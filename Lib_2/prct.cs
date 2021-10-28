b0c57120a65a6c034a7545df5f47062bc74980c2
исп-31 <-31@LAB24-02>
2021-10-28 15:42:29 +0300

    Добавьте файлы проекта.

---
+++ Lib_2/prct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lib_2
+{
+    /// <summary>
+    /// ИСП-31. Назаров Д. Вариант 2.
+    /// Вычислить разницу целых случайных чисел, распределенных в диапазоне от 2 до 10,
+    /// пока эта разница не станет меньше некоторого числа K(K<0). Вывести на экран
+    /// сгенерированные числа, значение суммы, и количество сгенерированных чисел
+    /// </summary>
+    public class Practice
+    {
+        public static void randomNumber(int minNumber, out int counter,  out int sum, out string allNumbers)
+        {
+            Random randomNumber = new Random();
+            sum = 0;
+            counter = 0;
+            
+            allNumbers = string.Empty;
+
+            do
+            {
+                int number = randomNumber.Next(2, 10);
+                allNumbers += number.ToString() + ", ";
+                sum -= number;
+                counter++;
+            }
+            while (sum > minNumber);
+        }
+        
+    }
+}
---
+++ Practic1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_2;
+
+namespace Practic1
+{
+    /// <summary>
+    /// ИСП-31. Назаров Д. Вариант 2.
+    /// Вычислить сумму целых случайных чисел, распределенных в диапазоне от 2 до 10, 
+    /// пока эта сумма не превышает некоторого числа K.Вывести на экран
+    /// сгенерированные числа, значение суммы, и количество сгенерированных чисел.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startProgramm_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            
+            bool isNotError = Int32.TryParse(enterMinSumOfNumbers.Text, out int minSumOfNumbers);
+            if (isNotError && minSumOfNumbers < 0)
+            {
+                string allNumbers = string.Empty;
+                int counter;
+                int sumNum;
+
+                minSumOfNumbers = Convert.ToInt32(enterMinSumOfNumbers.Text);
+
+                Practice.randomNumber(minSumOfNumbers, out counter, out sumNum, out allNumbers);
+
+                allRandomNumbers.Text = allNumbers;
+                numberCounter.Text = counter.ToString();
+                endSumOfNumbers.Text = sumNum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Число введено неверное или оно не отрицательное. \nВведите другое значение.", "Ошибка");
+                enterMinSumOfNumbers.Clear();
+            }
+
+        }
+        private void help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП-31. Назаров Д. Вариант 2. \nВычислить сумму целых случайных чисел, распределенных в диапазоне от 2 до 10, \nпока эта сумма не превышает некоторого числа K.Вывести на экран \nсгенерированные числа, значение суммы, и количество сгенерированных чисел." , "Данные о программе");
+        }
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void enterMinSumOfNumbers_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            allRandomNumbers.Clear();
+            endSumOfNumbers.Clear();
+            numberCounter.Clear();
+        }
+
+    }
+}
